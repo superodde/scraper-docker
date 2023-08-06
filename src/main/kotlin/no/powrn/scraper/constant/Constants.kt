f7f82df2b677b548b4636e37d73409fb9b6200b6
oddvin <oddvin.rosand@embriq.no>
2023-08-06 15:36:46 +0200

    SCRAPE-2 Deleted source code

--- src/main/kotlin/no/powrn/scraper/constant/Constants.kt
+++
@@ -1,6 +0,0 @@
-package no.powrn.scraper.constant
-
-const val ONE_MINUTE = 60000L
-const val FIVE_MINUTES = 300000L
-const val TEN_MINUTES = 600000L
-const val TWENTYFOUR_HOURS = 432000000L
--- src/main/kotlin/no/powrn/scraper/domain/Properties.kt
+++
@@ -1,6 +0,0 @@
-package no.powrn.scraper.domain
-
-data class Properties(
-    val id: String? = null,
-    val properties: MutableList<SingleProperty> = mutableListOf()
-)
